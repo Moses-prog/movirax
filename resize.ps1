8aed51a8f9956bc520f5883eb84a995eed32d80f
moses <mosesdimkpa1@gmail.com>
2026-09-05 19:49:52 +0100

    style: zoom icons by 35% to remove padding

--- resize.ps1
+++ resize.ps1
@@ -2,6 +2,8 @@ Add-Type -AssemblyName System.Drawing
 $iconPath = (Resolve-Path "src/app/favicon.ico").Path
 $bmp = [System.Drawing.Image]::FromFile($iconPath)
 
+$zoom = 1.35 # Zoom in by 35% to remove padding
+
 $files = Get-ChildItem -Path "public/icons" -Recurse -Filter *.png
 foreach ($file in $files) {
     if ($file.FullName -eq $iconPath) { continue }
@@ -14,11 +16,17 @@ foreach ($file in $files) {
         $newImg = New-Object System.Drawing.Bitmap($w, $h)
         $g = [System.Drawing.Graphics]::FromImage($newImg)
         $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
-        $g.DrawImage($bmp, 0, 0, $w, $h)
+        
+        $newW = [int]($w * $zoom)
+        $newH = [int]($h * $zoom)
+        $x = [int](($w - $newW) / 2)
+        $y = [int](($h - $newH) / 2)
+        
+        $g.DrawImage($bmp, $x, $y, $newW, $newH)
         $g.Dispose()
         $newImg.Save($file.FullName, [System.Drawing.Imaging.ImageFormat]::Png)
         $newImg.Dispose()
-        Write-Host "Updated $($file.Name) ($w x $h)"
+        Write-Host "Updated $($file.Name) ($w x $h) with zoom $zoom"
     } catch {
         Write-Host "Failed on $($file.Name): $_"
     }
