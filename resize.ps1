7b4ae90b9fdc2d54a7f41c3a161e83bf462bd495
moses <mosesdimkpa1@gmail.com>
2026-09-05 19:58:36 +0100

    style: undo bg color and zoom icon by 300%

--- resize.ps1
+++ resize.ps1
@@ -2,17 +2,12 @@ Add-Type -AssemblyName System.Drawing
 $iconPath = (Resolve-Path "src/app/favicon.ico").Path
 $bmp = [System.Drawing.Image]::FromFile($iconPath)
 
-$zoom = 1.45
-$bgColor = [System.Drawing.Color]::FromArgb(255, 13, 12, 15)
+$zoom = 3.0
 
 function Create-Icon {
     param($Path, $Size)
     $newImg = New-Object System.Drawing.Bitmap($Size, $Size)
     $g = [System.Drawing.Graphics]::FromImage($newImg)
-    
-    $brush = New-Object System.Drawing.SolidBrush($bgColor)
-    $g.FillRectangle($brush, 0, 0, $Size, $Size)
-    $brush.Dispose()
     
     $g.InterpolationMode = [System.Drawing.Drawing2D.InterpolationMode]::HighQualityBicubic
     
