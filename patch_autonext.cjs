0e7a1012c3835d851246a9b24fe8e86d4f779fbf
moses <mosesdimkpa1@gmail.com>
2026-09-22 00:41:59 +0100

    chore: cleanup

--- patch_autonext.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require("fs");
-let content = fs.readFileSync("src/utils/players.ts", "utf8");
-
-content = content.replace(/vidsrc\.(sh|me|in|pm)\/embed\/tv\/\$\{id\}\/\$\{season\}\/\$\{episode\}/g, "vidsrc.$1/embed/tv/${id}/${season}/${episode}?autonext=1");
-
-fs.writeFileSync("src/utils/players.ts", content);
-console.log("Added autonext=1 to vidsrc TV links");
