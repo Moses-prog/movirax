972fa5563a170bde208b6399aca2fbc1d35bd2f7
moses <mosesdimkpa1@gmail.com>
2026-09-21 23:17:47 +0100

    chore: cleanup

--- patch_manifest.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require("fs");
-let content = fs.readFileSync("public/manifest.json", "utf8");
-content = content.replace('"orientation": "portrait"', '"orientation": "any"');
-fs.writeFileSync("public/manifest.json", content);
-console.log("Successfully patched manifest.json!");
